6d982bf4b153826cec492ee4b3adb429d3585c13
DeanAkmal <118705266+DeanAkmal@users.noreply.github.com>
2022-11-30 10:40:21 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -15,3 +15,15 @@ if(input2 == "+"){
 
     console.log("Hasilnya adalah " + hasil);
 };
+
+ if(input2 == "-"){
+    const inputan1 = input.question("Enter Number ");
+    const inputan2 = input.question("Enter Number ");
+    
+    var nilai1 = Number(inputan1)
+    var nilai2 = Number(inputan2)
+
+    const hasil = nilai1 - nilai2;
+
+    console.log("Hasilnya adalah " + hasil);
+};
