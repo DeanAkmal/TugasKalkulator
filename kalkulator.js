c1adc2dc9e5e0f22ab17d43acb2ae928f90cf75d
DeanAkmal <118705266+DeanAkmal@users.noreply.github.com>
2022-11-30 10:29:04 +0700

    Update kalkulator.js

---
+++ kalkulator.js
@@ -0,0 +1,17 @@
+var input = require('readline-sync');
+console.log('--------------------------------');
+console.log( "Enter" + " + " + " - " + " * " + " or " + " / ");
+const input2 = input.question("Silahkan pilih salah satu ");
+console.log('--------------------------------');
+
+if(input2 == "+"){
+    const inputan1 = input.question("Enter Number ");
+    const inputan2 = input.question("Enter Number ");
+    
+    var nilai1 = Number(inputan1)
+    var nilai2 = Number(inputan2)
+
+    const hasil = nilai1 + nilai2;
+
+    console.log("Hasilnya adalah " + hasil);
+};
