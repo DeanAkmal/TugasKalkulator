dd746f6bc51493087e97d4505d6e8898898076b5
Sitipatimaah <118707249+Sitipatimaah@users.noreply.github.com>
2022-11-30 10:56:18 +0700

    Create kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -16,14 +16,30 @@ if(input2 == "+"){
     console.log("Hasilnya adalah " + hasil);
 };
 
- if(input2 == "-"){
-    const inputan1 = input.question("Enter Number ");
-    const inputan2 = input.question("Enter Number ");
-    
+if(input2 == "*"){
+   const inputan1 = input.question("Enter Number");
+   const inputan2 = input.question("Enter Number");
+
+   var nilai1 = Number(inputan1)
+   var nilai2 = Number(inputan2)
+
+   const hasil = nilai = nilai2;
+   
+   console.log("Hasilnya adalah" + hasil);
+};
+
+ if(input2 == "/"){
+    const inputan1 = input.question("Enter Number");
+    const inputan2 = input.question("Enter Number");
+
     var nilai1 = Number(inputan1)
     var nilai2 = Number(inputan2)
+  
+    cons hasil = nilai1 / nilai2;
 
-    const hasil = nilai1 - nilai2;
 
-    console.log("Hasilnya adalah " + hasil);
+    console.log("Hasilnya adalah" + hasil); 
 };
+   
+  
+  
